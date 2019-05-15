83b9afc83f7a27775bb829187ddb081bcaf8ce56
Brent Jackson <jxnblk@gmail.com>
2019-05-15 10:46:02 -0400

    Add linked headings

---
+++ docs/src/components/mdx-components.js
@@ -0,0 +1,28 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
+const heading = Tag => props => !!props.id ? (
+  <Tag {...props}>
+    <a href={`#${props.id}`}
+      css={{
+        color: 'inherit',
+        textDecoration: 'none',
+        ':hover': {
+          textDecoration: 'underline',
+        }
+      }}>
+      {props.children}
+    </a>
+  </Tag>
+) : (
+  <Tag {...props} />
+)
+
+export default {
+  h1: heading('h1'),
+  h2: heading('h2'),
+  h3: heading('h3'),
+  h4: heading('h4'),
+  h5: heading('h5'),
+  h6: heading('h6'),
+}
