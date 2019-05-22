d195a61aa5fa2aeeb64ba29f4a4e05013e9f9bf5
Brent Jackson <jxnblk@gmail.com>
2019-05-22 18:22:32 -0400

    Add prismjs example

---
+++ examples/prism/gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [
+          'gatsby-remark-prismjs',
+        ]
+      }
+    },
+  ]
+}
