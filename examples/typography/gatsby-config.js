e4c31e94a3a8340ad51ebeaea259b824aae82701
Brent Jackson <jxnblk@gmail.com>
2019-05-23 13:31:19 -0400

    Add typography example

---
+++ examples/typography/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          'Work+Sans',
+          'Quattrocento+Sans:400,400i,700'
+        ]
+      }
+    }
+  ]
+}
