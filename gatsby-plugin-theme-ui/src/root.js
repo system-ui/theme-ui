bcc8b89eccc76a2323190c3e3c7b7c83f26ae604
Brent Jackson <jxnblk@gmail.com>
2019-05-23 20:17:18 -0400

    Add support for color modes in gatsby plugin

--- gatsby-plugin-theme-ui/src/root.js
+++ gatsby-plugin-theme-ui/src/root.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { ThemeProvider } from 'theme-ui'
+import { ThemeProvider, ColorModeProvider } from 'theme-ui'
 import merge from 'lodash.merge'
 import { themes } from './theme-loader.js'
 
 const theme = merge({}, ...themes)
 
-export const wrapRootElement = ({ element, props }) =>
-  <ThemeProvider theme={theme}>
-    {element}
-  </ThemeProvider>
+export const wrapRootElement = ({ element, props }, opts) => {
+  if (opts.colorMode) {
+    <ColorModeProvider initialColorMode={opts.colorMode}>
+      <ThemeProvider theme={theme}>
+        {element}
+      </ThemeProvider>
+    </ColorModeProvider>
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      {element}
+    </ThemeProvider>
+  )
+}
