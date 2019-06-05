c046024fb9bd72edc6da6dec8c5d447c08190b23
Brent Jackson <jxnblk@gmail.com>
2019-06-05 13:42:16 -0400

    Add meta tags to docs

--- packages/docs/src/components/root.js
+++ packages/docs/src/components/root.js
@@ -11,6 +11,11 @@ export default props =>
       <meta name='description' content={pkg.description} />
       <link rel='icon' type='image/png' href='/icon.png' />
       <link rel='apple-touch-icon' type='image/png' href='/icon.png' />
+      <meta name='twitter:card' content='summary' />
+      <meta name='twitter:site' content='@jxnblk' />
+      <meta name='twitter:image' content='https://theme-ui.com/icon.png' />
+      <meta name='twitter:title' content='Theme UI' />
+      <meta name='twitter:description' content={pkg.description} />
     </Helmet>
     {props.children}
   </ThemeProvider>
