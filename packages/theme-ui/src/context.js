c448df90e45a62ce5c4613bf48de2c147524e5b9
Brent Jackson <jxnblk@gmail.com>
2019-06-17 14:46:47 -0400

    Make theme context stateful

--- packages/theme-ui/src/context.js
+++ packages/theme-ui/src/context.js
@@ -1,5 +1,14 @@
-import { createContext, useContext } from 'react'
+import {
+  createContext,
+  useContext,
+  useReducer
+} from 'react'
+import merge from 'lodash.merge'
+import { get } from '@styled-system/css'
+import styled from '@emotion/styled'
 import { components } from './components'
+import themed from './themed'
+import { useColorState } from './color-modes'
 
 export const Context = createContext({
   theme: {},
@@ -7,3 +16,38 @@ export const Context = createContext({
 })
 
 export const useThemeUI = () => useContext(Context)
+
+const merger = (state, next) => merge({}, state, next)
+
+const createComponents = (components = {}) => {
+  const next = {}
+  Object.keys(components).forEach(key => {
+    next[key] = styled(components[key])(themed(key))
+  })
+  return next
+}
+
+const applyColorMode = (theme, mode) => {
+  if (!mode) return theme
+  const modes = get(theme, 'colors.modes', {})
+  return merge({}, theme, {
+    colors: get(modes, mode, theme.colors)
+  })
+}
+
+export const useTheme = (opts) => {
+  const outer = useContext(Context)
+  const [ theme, setTheme ] = useReducer(merger, opts.theme || {})
+  const [ colorMode, setColorMode ] = useColorState(outer.colorMode || (opts.theme ? opts.theme.initialColorMode : undefined))
+
+  const context = merge({}, {
+    colorMode,
+    setColorMode,
+  }, outer, {
+    theme: applyColorMode(theme, outer.colorMode || colorMode),
+    setTheme,
+    components: createComponents(opts.components)
+  })
+
+  return context
+}
--- packages/theme-ui/src/core.js
+++ packages/theme-ui/src/core.js
@@ -1,43 +1,15 @@
-import styled from '@emotion/styled'
 import { ThemeContext as EmotionContext } from '@emotion/core'
 import { MDXProvider } from '@mdx-js/react'
 import { get } from '@styled-system/css'
-import merge from 'lodash.merge'
 import jsx from './jsx'
-import themed from './themed'
-import { Context, useThemeUI } from './context'
-import { useColorState } from './color-modes'
-
-const createComponents = (components = {}) => {
-  const next = {}
-  Object.keys(components).forEach(key => {
-    next[key] = styled(components[key])(themed(key))
-  })
-  return next
-}
+import { Context, useTheme } from './context'
 
 export const ThemeProvider = ({
   theme,
   components,
   ...props
 }) => {
-  const outer = useThemeUI()
-  const initialColorMode = outer.colorMode || (theme ? theme.initialColorMode : undefined)
-  const [ colorMode, setColorMode ] = useColorState(initialColorMode)
-  const context = merge({}, {
-    colorMode,
-    setColorMode,
-  }, outer, {
-    theme,
-    components: createComponents(components),
-  })
-
-  if (context.colorMode) {
-    const modes = get(context.theme, 'colors.modes', {})
-    context.theme = merge({}, context.theme, {
-      colors: get(modes, context.colorMode, context.theme.colors)
-    })
-  }
+  const context = useTheme({ theme, components })
 
   return (
     jsx(EmotionContext.Provider, { value: context.theme },
