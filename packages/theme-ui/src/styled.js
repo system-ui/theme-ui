437868071e9952edccd1066e6860b46fb46b443f
Brent Jackson <jxnblk@gmail.com>
2019-06-21 17:04:08 -0400

    Clean up styled code comments

--- packages/theme-ui/src/styled.js
+++ packages/theme-ui/src/styled.js
@@ -4,36 +4,24 @@ import isPropValid from '@emotion/is-prop-valid'
 import jsx from './jsx'
 
 export const styled = tag => (...args) => {
-  // should this use should-forward-prop?
-  const Styled = forwardRef(
-    (
-      {
-        as,
-        // these should only be applied when the outer element uses theme-ui jsx pragma
-        css,
-        sx,
-        ...props
-      },
-      ref
-    ) => {
-      const theme = useContext(ThemeContext)
-      let nextProps = {}
-      let styles = {}
-      args.forEach(arg => {
-        const style = typeof arg === 'function' ? arg({ theme, ...props }) : arg
-        Object.assign(styles, style)
-      })
-      for (let key in props) {
-        if (!isPropValid(key)) continue
-        nextProps[key] = props[key]
-      }
-
-      return jsx(as || tag, {
-        ...nextProps,
-        css: styles,
-      })
+  const Styled = forwardRef(({ as, ...props }, ref) => {
+    const theme = useContext(ThemeContext)
+    let nextProps = {}
+    let styles = {}
+    args.forEach(arg => {
+      const style = typeof arg === 'function' ? arg({ theme, ...props }) : arg
+      Object.assign(styles, style)
+    })
+    for (let key in props) {
+      if (!isPropValid(key)) continue
+      nextProps[key] = props[key]
     }
-  )
+
+    return jsx(as || tag, {
+      ...nextProps,
+      css: styles,
+    })
+  })
   return Styled
 }
 
