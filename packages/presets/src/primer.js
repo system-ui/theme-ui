0be8b1f25db1a0d40ae21caa1f6a003d7943795c
Brent Jackson <jxnblk@gmail.com>
2019-06-24 15:38:22 -0400

    Add primer preset

---
+++ packages/presets/src/primer.js
@@ -0,0 +1,11 @@
+import primerBase from 'primer-primitives'
+import base from './base'
+
+export const primer = {
+  ...primerBase,
+  styles: {
+    ...base.styles,
+  },
+}
+
+export default primer
