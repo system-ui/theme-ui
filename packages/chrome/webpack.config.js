1001d705c51bd429bb2a3e191b42a91641b2601b
Jordan Overbye <jordanoverbye@gmail.com>
2019-06-29 14:04:25 +1000

    Move devtool files to public

--- packages/chrome/webpack.config.js
+++ packages/chrome/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'bundle'),
+    path: path.resolve(__dirname, 'public', 'bundle'),
     filename: 'index.js',
   },
   module: {
