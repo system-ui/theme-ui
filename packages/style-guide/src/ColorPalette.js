43a2629053848836ea2997a51a577e8c6ca565c7
Jordan Overbye <jordanoverbye@gmail.com>
2019-07-01 17:44:50 +1000

    Update ColorPalette.js

--- packages/style-guide/src/ColorPalette.js
+++ packages/style-guide/src/ColorPalette.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { useThemeUI } from './context'
+import { useTheme } from './context'
 import ColorSwatch from './ColorSwatch'
 
 const join = (...args) => args.filter(Boolean).join('.')
