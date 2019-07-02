cdf8f8e4a3126da425ce8caaf49c523aee0ab1b1
John Otander <johnotander@gmail.com>
2019-07-02 13:37:32 -0600

    Update packages/gatsby-plugin-theme-ui/src/provider.js
    Co-Authored-By: Brent Jackson <jxnblk@gmail.com>

--- packages/gatsby-plugin-theme-ui/src/provider.js
+++ packages/gatsby-plugin-theme-ui/src/provider.js
@@ -4,7 +4,7 @@ import React from 'react'
 import theme from './index'
 import components from './components'
 
-export const wrapRootElement = ({ element }, _opts) =>
+export const wrapRootElement = ({ element }) =>
   jsx(
     ThemeProvider,
     {
