15be221ca4b80cf45199672d3d94611e920e70e3
Brent Jackson <jxnblk@gmail.com>
2019-07-17 15:28:01 -0400

    Update next.js example

---
+++ examples/next/pages/hello.js
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import { jsx, Styled } from 'theme-ui'
+
+export default props => (
+  <div
+    sx={{
+      maxWidth: 1024,
+      mx: 'auto',
+      p: 3,
+    }}>
+    <Styled.h1>Hello</Styled.h1>
+  </div>
+)
