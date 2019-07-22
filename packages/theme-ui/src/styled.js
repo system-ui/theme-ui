72fd3276759d1649e18115298bb17cc3effb77ad
Brent Jackson <jxnblk@gmail.com>
2019-07-22 11:20:48 -0400

    Forward props to functional components

--- packages/theme-ui/src/styled.js
+++ packages/theme-ui/src/styled.js
@@ -4,17 +4,21 @@ import isPropValid from '@emotion/is-prop-valid'
 import jsx from './jsx'
 
 export const styled = tag => (...args) => {
+  const shouldForwardProps = typeof tag === 'function'
   const Styled = forwardRef(({ as, ...props }, ref) => {
     const theme = useContext(ThemeContext)
-    let nextProps = {}
+    let nextProps = shouldForwardProps ? props : {}
     let styles = {}
     args.forEach(arg => {
       const style = typeof arg === 'function' ? arg({ theme, ...props }) : arg
       Object.assign(styles, style)
     })
-    for (let key in props) {
-      if (!isPropValid(key)) continue
-      nextProps[key] = props[key]
+
+    if (!shouldForwardProps) {
+      for (let key in props) {
+        if (!isPropValid(key)) continue
+        nextProps[key] = props[key]
+      }
     }
 
     return jsx(as || tag, {
