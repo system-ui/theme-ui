b2da8e18f5c6711a5a831b19aa847a07da049bf8
Brent Jackson <jxnblk@gmail.com>
2019-07-22 16:24:04 -0400

    Add testimonials

--- packages/docs/src/components/tiles.js
+++ packages/docs/src/components/tiles.js
@@ -1,10 +1,18 @@
 /** @jsx jsx */
 import { jsx, ThemeProvider, Container } from 'theme-ui'
 
-export default props => (
+export default ({ columns = 3, ...props }) => (
   <ThemeProvider
     theme={{
       styles: {
+        ol: {
+          listStyle: 'none',
+          display: ['block', 'grid'],
+          gridTemplateColumns: ['auto', `repeat(${columns}, 1fr)`],
+          gridGap: 24,
+          p: 0,
+          m: 0,
+        },
         ul: {
           listStyle: 'none',
           display: ['block', 'grid'],
