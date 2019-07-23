9c36006a9d068ed15d19940b7fe8a2a8a733e501
John Otander <johnotander@gmail.com>
2019-07-23 14:18:29 -0600

    Add some missing bootstrap styles

--- packages/presets/src/bootstrap.js
+++ packages/presets/src/bootstrap.js
@@ -172,6 +172,35 @@ export const styles = {
   },
   table: {
     // todo
+    width: '100%',
+    marginBottom: 3,
+    color: 'gray.9',
+    borderCollapse: 'collapse',
+  },
+  th: {
+    verticalAlign: 'bottom',
+    borderTopWidth: 2,
+    borderTopStyle: 'solid',
+    borderTopColor: 'gray.3',
+    borderBottomWidth: 2,
+    borderBottomStyle: 'solid',
+    borderBottomColor: 'gray.3',
+    padding: '.75rem',
+    textAlign: 'inherit',
+  },
+  td: {
+    borderBottomWidth: 2,
+    borderBottomStyle: 'solid',
+    borderBottomColor: 'gray.3',
+    verticalAlign: 'top',
+    padding: '.75rem',
+  },
+  inlineCode: {
+    color: 'pink',
+  },
+  img: {
+    maxWidth: '100%',
+    height: 'auto',
   },
 }
 
