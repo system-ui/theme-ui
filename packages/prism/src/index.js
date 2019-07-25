a5d95d0fbe91c28ad3fff4c10a521bf8a383c058
John Letey <30328854+johnletey@users.noreply.github.com>
2019-07-25 13:04:54 +0100

    feat: Add support for code titles (system-ui#212)

--- packages/prism/src/index.js
+++ packages/prism/src/index.js
@@ -6,7 +6,16 @@ import { jsx, Styled } from 'theme-ui'
 
 import Prism from 'prismjs/components/prism-core'
 
-export default ({ children, className: outerClassName, title, prism }) => {
+import {
+  colors,
+  fontSizes as fontSizeTokens,
+  space as spaceTokens,
+} from 'gatsby-design-tokens'
+
+const fontSizes = fontSizeTokens.map(token => `${token / 16}rem`)
+const space = spaceTokens.map(token => `${token / 16}rem`)
+
+export default ({ children, className: outerClassName, title = ``, prism }) => {
   const [language] = outerClassName.replace(/language-/, '').split(' ')
 
   return (
@@ -16,6 +25,18 @@ export default ({ children, className: outerClassName, title, prism }) => {
       code={children.trim()}
       language={language}
       theme={undefined}>
+      {title && (
+        <div
+          sx={{
+            background: colors.code.bg,
+            borderBottom: `1px solid ${colors.code.border}`,
+            color: colors.code.text,
+            padding: `${space[5]} ${space[6]} ${space[4]}`,
+            fontSize: fontSizes[0],
+          }}>
+          <div sx={{ fontSize: fontSizes[0] }}>{title}</div>
+        </div>
+      )}
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <Styled.pre className={`${outerClassName} ${className}`} style={style}>
           {tokens.map((line, i) => (
