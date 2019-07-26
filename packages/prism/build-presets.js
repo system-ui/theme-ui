d8844db86596958c0c2460e8cd0a013cc54d1e23
Brent Jackson <jxnblk@gmail.com>
2019-07-26 14:56:19 -0400

    Add react prism presets

--- packages/prism/build-presets.js
+++ packages/prism/build-presets.js
@@ -7,32 +7,30 @@ const dir = {
   prismjs: path.join(path.dirname(require.resolve('prismjs')), 'themes'),
 }
 
-const css = {
-  prism: [
-    'prism-coy.css',
-    'prism-dark.css',
-    'prism-funky.css',
-    'prism-okaidia.css',
-    'prism-solarizedlight.css',
-    'prism-tomorrow.css',
-    'prism-twilight.css',
-    'prism.css',
-  ],
-}
-
 const ignore = /^(pre|code|@media|\:not)/
 const cleanObject = obj => {
   let next = {}
   Object.entries(obj).forEach(([key, value]) => {
     if (ignore.test(key)) return
-    next[key] = value
+    const cleanKey = key.replace(/\n/g, '').replace(/\.token/g, '')
+
+    next[cleanKey] = value
   })
   return next
 }
 
 const cleanName = str => str.replace(/\.css$/, '')
 
-const contents = css.prism.map(name => {
+const prism = [
+  'prism-coy.css',
+  'prism-dark.css',
+  'prism-funky.css',
+  'prism-okaidia.css',
+  'prism-solarizedlight.css',
+  'prism-tomorrow.css',
+  'prism-twilight.css',
+  'prism.css',
+].map(name => {
   const filename = path.join(dir.prismjs, name)
   const content = fs.readFileSync(filename, 'utf8')
   const tree = postcss.parse(content)
@@ -46,7 +44,40 @@ const contents = css.prism.map(name => {
   }
 })
 
-contents.forEach(preset => {
+const prismReactToStyles = obj => {
+  let styles = {
+    ...obj.plain,
+  }
+  obj.styles.forEach(s => {
+    const selector = s.types.map(t => '.' + t).join(',')
+    styles[selector] = s.style
+  })
+  return styles
+}
+
+const reactPrism = [
+  'dracula',
+  'duotoneDark',
+  'duotoneLight',
+  'nightOwl',
+  'nightOwlLight',
+  'oceanicNext',
+  'ultramin',
+  'github',
+  'shadesOfPurple',
+  'vsDark',
+].map(name => {
+  const raw = require('prism-react-renderer/themes/' + name)
+  const styles = prismReactToStyles(raw)
+  return {
+    name,
+    raw,
+    styles,
+  }
+})
+
+const presets = [...prism, ...reactPrism]
+presets.forEach(preset => {
   const outfile = path.join(__dirname, 'presets', preset.name + '.json')
   const json = JSON.stringify(preset.styles, null, 2)
   fs.writeFileSync(outfile, json)
