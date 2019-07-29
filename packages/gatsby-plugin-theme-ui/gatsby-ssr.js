c275a7b930ff4ea0790de6f642accbb8d80a7ac4
Brent Jackson <jxnblk@gmail.com>
2019-07-29 17:59:06 -0400

    Add key to script element in gatsby-plugin-theme-ui

--- packages/gatsby-plugin-theme-ui/gatsby-ssr.js
+++ packages/gatsby-plugin-theme-ui/gatsby-ssr.js
@@ -14,6 +14,7 @@ const noflash = `
 
 export const onRenderBody = ({ setPreBodyComponents }) => {
   const script = jsx('script', {
+    key: 'theme-ui-noscript',
     dangerouslySetInnerHTML: {
       __html: noflash,
     },
