d397348ca9a72ab1d62b487d65c4840bf203a854
John Otander <johnotander@gmail.com>
2019-07-30 10:57:18 -0600

    Remove unused transition

--- packages/docs/src/components/preset.js
+++ packages/docs/src/components/preset.js
@@ -17,49 +17,42 @@ export default ({ preset: presetName }) => {
 
   return (
     <div>
-      <div
-        sx={{
-          '*': {
-            transition: 'all .2s ease-out',
-          },
-        }}>
-        <ColorMode />
-        <ThemeContext.Provider value={preset}>
-          <Styled.root>
-            <Styled.h2>Colors</Styled.h2>
-            <ColorPalette omit={['modes', 'header']} />
-            <Styled.h2>Typography</Styled.h2>
-            <TypeStyle fontSize={7}>
-              Body: <FontFamily name="body" />
-            </TypeStyle>
-            <HeadingStyle
-              fontFamily="heading"
-              fontWeight="heading"
-              lineHeight="heading"
-              fontSize={7}>
-              Heading: <FontFamily name="heading" />
-            </HeadingStyle>
-            <Styled.h2>Type Scale</Styled.h2>
-            <TypeScale />
-            <MDXProvider components={components}>
-              <Lorem />
-            </MDXProvider>
-            <Styled.h2>Raw JSON</Styled.h2>
-            <textarea
-              value={JSON.stringify(preset, null, 2)}
-              rows={16}
-              readOnly
-              sx={{
-                width: '100%',
-                fontFamily: 'monospace',
-                bg: 'muted',
-                border: 0,
-                borderRadius: 4,
-              }}
-            />
-          </Styled.root>
-        </ThemeContext.Provider>
-      </div>
+      <ColorMode />
+      <ThemeContext.Provider value={preset}>
+        <Styled.root>
+          <Styled.h2>Colors</Styled.h2>
+          <ColorPalette omit={['modes', 'header']} />
+          <Styled.h2>Typography</Styled.h2>
+          <TypeStyle fontSize={7}>
+            Body: <FontFamily name="body" />
+          </TypeStyle>
+          <HeadingStyle
+            fontFamily="heading"
+            fontWeight="heading"
+            lineHeight="heading"
+            fontSize={7}>
+            Heading: <FontFamily name="heading" />
+          </HeadingStyle>
+          <Styled.h2>Type Scale</Styled.h2>
+          <TypeScale />
+          <MDXProvider components={components}>
+            <Lorem />
+          </MDXProvider>
+          <Styled.h2>Raw JSON</Styled.h2>
+          <textarea
+            value={JSON.stringify(preset, null, 2)}
+            rows={16}
+            readOnly
+            sx={{
+              width: '100%',
+              fontFamily: 'monospace',
+              bg: 'muted',
+              border: 0,
+              borderRadius: 4,
+            }}
+          />
+        </Styled.root>
+      </ThemeContext.Provider>
     </div>
   )
 }
