1be9af479f50d722be27f41fec83a55097da1b21
Jordan Overbye <jordanoverbye@gmail.com>
2019-08-07 17:24:00 +1000

    gatsby-plugin-theme-ui use applyColorModeFromLocalStorage
    Signed-off-by: Jordan Overbye <jordanoverbye@gmail.com>

--- packages/gatsby-plugin-theme-ui/gatsby-ssr.js
+++ packages/gatsby-plugin-theme-ui/gatsby-ssr.js
@@ -1,22 +1,12 @@
-import { jsx } from 'theme-ui'
+import { jsx, applyColorModeFromLocalStorage } from 'theme-ui'
 
 export { wrapRootElement } from './src/provider'
-
-const noflash = `
-(function() {
-  try {
-    var mode = localStorage.getItem('theme-ui-color-mode');
-    if (!mode) return
-    document.body.classList.add('theme-ui-' + mode);
-  } catch (e) {}
-})();
-`
 
 export const onRenderBody = ({ setPreBodyComponents }) => {
   const script = jsx('script', {
     key: 'theme-ui-noscript',
     dangerouslySetInnerHTML: {
-      __html: noflash,
+      __html: applyColorModeFromLocalStorage,
     },
   })
   setPreBodyComponents([script])
