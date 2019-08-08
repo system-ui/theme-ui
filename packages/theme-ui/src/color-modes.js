55351a9ce2a0c4da16cd18bed6192963bce1270e
Jordan Overbye <jordanoverbye@gmail.com>
2019-08-08 17:26:06 +1000

    Indentation
    Signed-off-by: Jordan Overbye <jordanoverbye@gmail.com>

--- packages/theme-ui/src/color-modes.js
+++ packages/theme-ui/src/color-modes.js
@@ -72,13 +72,11 @@ export const ApplyColorModeFromLocalStorage = () => (
   <script
     key="theme-ui-noscript"
     dangerouslySetInnerHTML={{
-      __html: `(function() {
-    try {
-      var mode = localStorage.getItem('theme-ui-color-mode');
-      if (!mode) return
-      document.body.classList.add('theme-ui-' + mode);
-    } catch (e) {}
-  })();`,
+      __html: `(function() { try {
+        var mode = localStorage.getItem('theme-ui-color-mode');
+        if (!mode) return
+        document.body.classList.add('theme-ui-' + mode);
+      } catch (e) {} })();`,
     }}
   />
 )
