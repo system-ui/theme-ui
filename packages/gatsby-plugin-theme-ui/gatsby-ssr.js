c48ec8127fb788060c316440d0b758b952564795
Jordan Overbye <jordanoverbye@gmail.com>
2019-08-08 17:22:22 +1000

    Remove console.log
    Signed-off-by: Jordan Overbye <jordanoverbye@gmail.com>

--- packages/gatsby-plugin-theme-ui/gatsby-ssr.js
+++ packages/gatsby-plugin-theme-ui/gatsby-ssr.js
@@ -3,6 +3,5 @@ import { ApplyColorModeFromLocalStorage } from 'theme-ui'
 export { wrapRootElement } from './src/provider'
 
 export const onRenderBody = ({ setPreBodyComponents }) => {
-  console.log(ApplyColorModeFromLocalStorage)
   setPreBodyComponents([ApplyColorModeFromLocalStorage])
 }
