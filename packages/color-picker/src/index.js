54333457a2ff1d7092fd1e422bffd66ee0fb5128
Brent Jackson <jxnblk@gmail.com>
2019-08-27 11:51:11 -0400

    Edit color picker

--- packages/color-picker/src/index.js
+++ packages/color-picker/src/index.js
@@ -1,24 +1,49 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { CustomPicker, Hue } from 'react-color'
+import { CustomPicker } from 'react-color'
+import {
+  Hue,
+  Saturation,
+  EditableInput,
+} from 'react-color/lib/components/common'
 
-const Box = props => (
+const Box = ({ height = 16, ...props }) => (
   <div
     {...props}
     sx={{
       position: 'relative',
+      width: 256,
+      height,
     }}
   />
 )
 
+const Lens = props => <div {...props} sx={{}} />
+
+const Handle = props => <div {...props} sx={{}} />
+
 export default CustomPicker(props => {
-  console.log(props)
   return (
-    <div>
-      ColorPicker
-      <Box>
+    <div
+      sx={{
+        display: 'grid',
+        gridGap: 2,
+      }}>
+      <Box height={256}>
+        <Saturation {...props} />
+      </Box>
+      <Box height={8}>
         <Hue {...props} />
       </Box>
+      <EditableInput
+        {...props}
+        value={props.hex}
+        sx={{
+          width: '100%',
+          fontSize: 'inherit',
+          fontFamily: 'inherit',
+        }}
+      />
     </div>
   )
 })
--- packages/docs/src/pages/color-picker.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import ColorPicker from '@theme-ui/color-picker'
-
-export default () => <div>hi</div>
