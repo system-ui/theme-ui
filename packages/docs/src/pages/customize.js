35e07873e57cd608adf6f1c9e376b67f42117035
Brent Jackson <jxnblk@gmail.com>
2019-09-04 16:13:25 -0400

    Edit customize page

--- packages/docs/src/pages/customize.js
+++ packages/docs/src/pages/customize.js
@@ -14,6 +14,9 @@ import { TypeStyle, FontFamily } from '@theme-ui/style-guide'
 import { useReducer } from 'react'
 import merge from 'lodash.merge'
 import * as presets from '@theme-ui/presets'
+import copy from 'copy-to-clipboard'
+import stringify from 'stringify-object'
+import Button from '../components/button'
 
 const reducer = (state, next) => merge({}, state, next)
 
@@ -24,6 +27,10 @@ export default props => {
     theme,
     setTheme,
   }
+
+  const json = stringify(context.theme, {
+    indent: '  '
+  })
 
   return (
     <div>
@@ -57,13 +64,13 @@ export default props => {
           </div>
         </ThemeProvider>
         <Fonts />
-        <div sx={{ my: 3 }}>
+        <div sx={{ my: 2 }}>
           <b>Font Sizes</b>
           <Row>
             <FontSizes />
           </Row>
         </div>
-        <Row sx={{ my: 3 }}>
+        <Row sx={{ my: 2 }}>
           <div>
             <b>Font Weights</b>
             <Row>
@@ -77,15 +84,24 @@ export default props => {
             </Row>
           </div>
         </Row>
+        <div sx={{ my: 2 }}>
+          <b>Space</b>
+          <Row>
+            <Space />
+          </Row>
+        </div>
         <p>
           Note: some web fonts may not render unless installed locally.
         </p>
       </Editor>
-      <Styled.h3>
-        JSON
-      </Styled.h3>
+      <Button
+        onClick={e => {
+          copy(json)
+        }}>
+        Copy Theme
+      </Button>
       <Styled.pre
-        children={JSON.stringify(theme, null, 2)}
+        children={json}
         sx={{
           maxHeight: 512,
           overflowY: 'auto',
