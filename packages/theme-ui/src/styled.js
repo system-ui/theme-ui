d9020340d2af281216b86f408395655ce5709bac
Brent Jackson <jxnblk@gmail.com>
2019-09-19 12:51:37 -0400

    Remove console

--- packages/theme-ui/src/styled.js
+++ packages/theme-ui/src/styled.js
@@ -7,11 +7,6 @@ export const styled = tag => (...args) => {
   const Styled = forwardRef(({ as, ...props }, ref) => {
     const shouldForwardProps =
       typeof tag !== 'string' || (as && typeof as !== 'string')
-    console.log({
-      as,
-      shouldForwardProps,
-      props,
-    })
     const theme = useContext(ThemeContext)
     let nextProps = shouldForwardProps ? props : {}
     let styles = {}
