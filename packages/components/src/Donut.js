5c723831f01a361d5ec03a1c1326ef0749d39174
Brent Jackson <jxnblk@gmail.com>
2019-11-12 15:05:40 -0500

    Adjust Donut component

--- packages/components/src/Donut.js
+++ packages/components/src/Donut.js
@@ -3,13 +3,16 @@ import Box from './Box'
 
 export const Donut = React.forwardRef(({
   size = 128,
-  strokeWidth = 4,
+  strokeWidth = 2,
   value = 0,
+  min = 0,
+  max = 1,
+  title,
   ...props
 }, ref) => {
-  const r = 16 - (strokeWidth / 2)
-  const n = 2 * Math.PI * r
-  const offset = n - (value * n)
+  const r = 16 - (strokeWidth)
+  const C = 2 * r * Math.PI
+  const offset = C - ((value - min) / (max - min) * C)
 
   return (
     <Box
@@ -19,26 +22,29 @@ export const Donut = React.forwardRef(({
       width={size}
       height={size}
       strokeWidth={strokeWidth}
-      stroke='currentcolor'
       fill='none'
+      stroke='currentcolor'
+      role='img'
+      aria-valuenow={value}
+      aria-valuemin={min}
+      aria-valuemax={max}
       {...props}
       __css={{
         color: 'primary',
       }}>
+      {title && <title>{title}</title>}
       <circle
         cx={16}
         cy={16}
         r={r}
         opacity={1/8}
-        stroke='#eee'
       />
       <circle
         cx={16}
         cy={16}
         r={r}
-        strokeWidth={strokeWidth}
-        strokeDashArray={n}
-        strokeDashOffset={offset}
+        strokeDasharray={C}
+        strokeDashoffset={offset}
         transform='rotate(-90 16 16)'
       />
     </Box>
