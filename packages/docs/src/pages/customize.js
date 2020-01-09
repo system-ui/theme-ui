36610e310b6dcabcf58b1aa839f553bfd8a2b23b
Brent Jackson <jxnblk@gmail.com>
2020-01-09 17:29:56 -0500

    Adjust customize page

--- packages/docs/src/pages/customize.js
+++ packages/docs/src/pages/customize.js
@@ -1,14 +1,15 @@
 /** @jsx jsx */
-import { jsx, Styled, ThemeProvider } from 'theme-ui'
+import { jsx, Styled, ThemeProvider, Grid } from 'theme-ui'
 import {
-  Editor,
-  ColorPalette,
-  Fonts,
-  FontWeights,
-  LineHeights,
-  FontSizes,
-  Space,
-  Row,
+  EditorProvider,
+  Theme,
+  // ColorPalette,
+  // Fonts,
+  // FontWeights,
+  // LineHeights,
+  // FontSizes,
+  // Space,
+  // Row,
 } from '@theme-ui/editor'
 import { TypeStyle, FontFamily } from '@theme-ui/style-guide'
 import { useReducer } from 'react'
@@ -21,82 +22,71 @@ import Button from '../components/button'
 const reducer = (state, next) => merge({}, state, next)
 
 export default props => {
-  const [ theme, setTheme ] = useReducer(reducer, {...presets.base})
-
-  const context = {
-    theme,
-    setTheme,
-  }
-
-  const json = stringify(context.theme, {
-    indent: '  '
-  })
+  // TODO
+  // const [ theme, setTheme ] = useReducer(reducer, {...presets.base})
+  // const context = { theme, setTheme, }
+  // const json = stringify(context.theme, { indent: '  ' })
 
   return (
     <div>
       <Styled.h1>
         Create a Custom Theme
       </Styled.h1>
-      <Editor
-        fontSize={12}
-        context={context}>
+      <EditorProvider theme={presets.base}>
         <b>Colors</b>
-        <ColorPalette
-          size={64}
-        />
-        <ThemeProvider theme={context.theme}>
-          <div
-            sx={{
-              my: 4,
-              fontFamily: 'body',
-              lineHeight: 'body',
-              fontWeight: 'body',
-              color: 'text',
-              bg: 'background',
-            }}>
-            <TypeStyle
-              fontFamily='heading'
-              lineHeight='heading'
-              fontWeight='heading'
-              fontSize={[ 5, 6 ]}>
-              Aa <FontFamily name='heading' />
-            </TypeStyle>
-            <TypeStyle fontSize={3}>
-              Aa <FontFamily name='body' />
-            </TypeStyle>
-          </div>
-        </ThemeProvider>
-        <Fonts />
+        <Theme.Colors size={64} />
+        <div
+          sx={{
+            my: 4,
+            fontFamily: 'body',
+            lineHeight: 'body',
+            fontWeight: 'body',
+            color: 'text',
+            bg: 'background',
+          }}>
+          <TypeStyle
+            fontFamily='heading'
+            lineHeight='heading'
+            fontWeight='heading'
+            fontSize={[ 5, 6 ]}>
+            Aa <FontFamily name='heading' />
+          </TypeStyle>
+          <TypeStyle fontSize={3}>
+            Aa <FontFamily name='body' />
+          </TypeStyle>
+        </div>
+        <Theme.Fonts />
         <div sx={{ my: 2 }}>
           <b>Font Sizes</b>
-          <Row>
-            <FontSizes />
-          </Row>
+          <Grid>
+            <Theme.FontSizes />
+          </Grid>
         </div>
-        <Row sx={{ my: 2 }}>
+        <div sx={{ my: 2 }}>
           <div>
             <b>Font Weights</b>
-            <Row>
-              <FontWeights />
-            </Row>
+            <Grid>
+              <Theme.FontWeights />
+            </Grid>
           </div>
           <div>
             <b>Line Heights</b>
-            <Row>
-              <LineHeights />
-            </Row>
+            <Grid>
+              <Theme.LineHeights />
+            </Grid>
           </div>
-        </Row>
+        </div>
         <div sx={{ my: 2 }}>
           <b>Space</b>
-          <Row>
-            <Space />
-          </Row>
+          <Grid>
+            <Theme.Space />
+          </Grid>
         </div>
         <p>
           Note: some web fonts may not render unless installed locally.
         </p>
-      </Editor>
+      </EditorProvider>
+      {/* TODO
       <Button
         onClick={e => {
           copy(json)
@@ -110,6 +100,7 @@ export default props => {
           overflowY: 'auto',
         }}
       />
+      */}
     </div>
   )
 }
