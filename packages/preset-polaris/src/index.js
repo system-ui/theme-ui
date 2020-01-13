a38b11b5d9903b3a7c430dbb20b030cb547f5b2b
Yuraima <yurm04@gmail.com>
2020-01-12 19:48:03 -0500

    Add theme preset polaris

---
+++ packages/preset-polaris/src/index.js
@@ -0,0 +1,33 @@
+import base from '@theme-ui/preset-base'
+
+const text = '"San Francisco", Roboto, "Segoe UI", "Helvetica Neue", sans-serif'
+export const polaris = {
+  ...base,
+  colors: {
+    text: '#454f5b',
+    background: '#fff',
+    primary: '#5C6AC4',
+    secondary: '#006FBB',
+    highlight: '#47C1BF',
+    muted: '#e6e6e6',
+    gray: '#DFE3E8',
+    accent: '#F49342',
+    darken: '#00044C',
+  },
+  fonts: {
+    body: text,
+    heading: text,
+    moonospace: 'Menlo, monospace',
+  },
+  fontWeights: {
+    body: 400,
+    heading: 600,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.75,
+    heading: 1.25,
+  },
+}
+
+export default polaris
