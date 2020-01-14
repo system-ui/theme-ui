ae1ac8a090a2b28e945e81250f89e243a3d37817
Alex Permyakov <alex.permyakoff@gmail.com>
2020-01-14 21:06:36 +0100

    hide other changes

--- packages/theme-ui/src/color-modes.js
+++ packages/theme-ui/src/color-modes.js
@@ -5,18 +5,12 @@ import { useThemeUI } from './context'
 import { createColorStyles } from './custom-properties'
 
 const STORAGE_KEY = 'theme-ui-color-mode'
-const hasStorage = () => {
-  try {
-    return typeof window.localStorage !== undefined
-  } catch (e) {
-    return false
-  }
-}
+const HAS_STORAGE = typeof Storage !== 'undefined'
 
 const storage = {
   get: init =>
-    (hasStorage() && window.localStorage.getItem(STORAGE_KEY)) || init,
-  set: value => hasStorage() && window.localStorage.setItem(STORAGE_KEY, value),
+    (HAS_STORAGE && window.localStorage.getItem(STORAGE_KEY)) || init,
+  set: value => HAS_STORAGE && window.localStorage.setItem(STORAGE_KEY, value),
 }
 
 export const getMediaQuery = () => {
