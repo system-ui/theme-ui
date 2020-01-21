469dcbcfde062f890e13a1ec1a46f280739d0ba5
Alex Permyakov <alex.permyakoff@gmail.com>
2020-01-21 20:56:09 +0100

    add more spinners

--- packages/components/src/Spinner.js
+++ packages/components/src/Spinner.js
@@ -2,6 +2,14 @@ import React from 'react'
 import { keyframes } from '@emotion/core'
 import Box from './Box'
 
+const range = n => {
+  const v = []
+  for (let i = 0; i < n; i++) {
+    v.push(i)
+  }
+  return v
+}
+
 const spin = keyframes({
   from: {
     transform: 'rotate(0deg)',
@@ -11,56 +19,145 @@ const spin = keyframes({
   },
 })
 
-export const Spinner = React.forwardRef(({
-  size = 48,
-  strokeWidth = 4,
-  max = 1,
-  title = 'Loading...',
-  duration = 500,
-  ...props
-}, ref) => {
-  const r = 16 - (strokeWidth)
-  const C = 2 * r * Math.PI
-  const offset = C - (1/4 * C)
+const roller = keyframes({
+  '0%, 20%, 80%, 100%': {
+    transform: 'scale(1)',
+  },
+  '50%': {
+    transform: 'scale(1.5)',
+  },
+})
+
+const facebook = keyframes({
+  '0%': {
+    top: '8px',
+    height: '48px',
+  },
+  '50%, 100%': {
+    top: '24px',
+    height: '24px',
+  },
+})
+
+const animations = { spin, roller, facebook }
+
+export const Spinner = props => {
+  const {
+    animation = 'spin',
+    duration = 600,
+    width = 48,
+    height = 48,
+    items = 1,
+    delay,
+    itemX = () => 0,
+    itemY = () => 0,
+    itemWidth,
+    itemHeight,
+  } = props
 
   return (
     <Box
-      ref={ref}
-      as='svg'
-      viewBox='0 0 32 32'
-      width={size}
-      height={size}
-      strokeWidth={strokeWidth}
-      fill='none'
-      stroke='currentcolor'
-      role='img'
+      role="img"
       {...props}
-      __css={{
-        color: 'primary',
-        overflow: 'visible',
+      __themeKey="spinners"
+      sx={{
+        position: 'relative',
+        display: 'inline-block',
+        width: `${width}px`,
+        height: `${height}px`,
       }}>
-      <title>{title}</title>
-      <circle
-        cx={16}
-        cy={16}
-        r={r}
-        opacity={1/8}
-      />
-      <Box
-        as='circle'
-        cx={16}
-        cy={16}
-        r={r}
-        strokeDasharray={C}
-        strokeDashoffset={offset}
-        __css={{
-          transformOrigin: '50% 50%',
-          animationName: spin.toString(),
-          animationTimingFunction: 'linear',
-          animationDuration: duration + 'ms',
-          animationIterationCount: 'infinite',
-        }}
-      />
+      {range(items).map(index => {
+        const cfg = { ...props, index, width, height }
+        return (
+          <Box
+            key={index}
+            sx={{
+              position: 'absolute',
+              display: 'block',
+              width: `${itemWidth || width}px`,
+              height: `${itemHeight || height}px`,
+              animationIterationCount: 'infinite',
+              animationName: animations[animation].toString(),
+              animationDuration: `${duration}ms`,
+              animationDelay: `${(delay || 0) * index}ms`,
+              top: `${itemY(cfg)}px`,
+              left: `${itemX(cfg)}px`,
+            }}
+          />
+        )
+      })}
     </Box>
   )
-})
+}
+
+export const DualringSpinner = ({ duration }) => (
+  <Spinner
+    animation="spin"
+    duration={duration}
+    delay={-150}
+    items={4}
+    __css={{
+      borderRadius: '50%',
+      borderWidth: 6,
+      borderStyle: 'solid',
+      borderColor: 'rgba(51, 51, 238, 0.1)',
+      '& > div': {
+        content: '" "',
+        borderRadius: '50%',
+        left: `-6px`,
+        top: `-6px`,
+        borderWidth: 6,
+        borderStyle: 'solid',
+        borderColor: `#3333ee transparent transparent transparent`,
+        animationTimingFunction: 'cubic-bezier(0.5, 0, 0.5, 1)',
+      },
+    }}
+  />
+)
+
+export const FacebookSpinner = ({ duration, itemWidth, itemHeight }) => (
+  <Spinner
+    duration={duration}
+    items={3}
+    itemWidth={itemWidth}
+    itemHeight={itemHeight}
+    animation="facebook"
+    delay={100}
+    itemX={({ index, itemWidth }) => (8 + itemWidth) * index}
+    __css={{
+      '& > div': {
+        display: 'inline-block',
+        background: '#3333ee',
+        animationTimingFunction: 'cubic-bezier(0, 0.5, 0.5, 1)',
+      },
+    }}
+  />
+)
+
+export const RollerSpinner = ({ duration, items, itemWidth, itemHeight }) => (
+  <Spinner
+    duration={duration}
+    items={items}
+    animation="roller"
+    delay={100}
+    itemX={({ index, items, width }) => {
+      const r = width / 2
+      const angle = (index / items) * Math.PI * 2
+      return r + r * Math.cos(angle)
+    }}
+    itemY={({ index, items, height }) => {
+      const r = height / 2
+      const angle = (index / items) * Math.PI * 2
+      return r + r * Math.sin(angle)
+    }}
+    itemWidth={itemWidth}
+    itemHeight={itemHeight}
+    __css={{
+      '& > div': {
+        background: '#3333ee',
+        borderRadius: '50%',
+        animationTimingFunction: 'linear',
+      },
+    }}
+  />
+)
