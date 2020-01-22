049ca436a77ebff79de23e244d17dc7f72f6c5de
Alex Permyakov <alex.permyakoff@gmail.com>
2020-01-22 17:20:10 +0100

    hide spinners

--- packages/components/src/Spinner.js
+++ packages/components/src/Spinner.js
@@ -11,56 +11,56 @@ const spin = keyframes({
   },
 })
 
-export const Spinner = React.forwardRef(
-  (
-    {
-      size = 48,
-      strokeWidth = 4,
-      max = 1,
-      title = 'Loading...',
-      duration = 500,
-      ...props
-    },
-    ref
-  ) => {
-    const r = 16 - strokeWidth
-    const C = 2 * r * Math.PI
-    const offset = C - (1 / 4) * C
+export const Spinner = React.forwardRef(({
+  size = 48,
+  strokeWidth = 4,
+  max = 1,
+  title = 'Loading...',
+  duration = 500,
+  ...props
+}, ref) => {
+  const r = 16 - (strokeWidth)
+  const C = 2 * r * Math.PI
+  const offset = C - (1/4 * C)
 
-    return (
+  return (
+    <Box
+      ref={ref}
+      as='svg'
+      viewBox='0 0 32 32'
+      width={size}
+      height={size}
+      strokeWidth={strokeWidth}
+      fill='none'
+      stroke='currentcolor'
+      role='img'
+      {...props}
+      __css={{
+        color: 'primary',
+        overflow: 'visible',
+      }}>
+      <title>{title}</title>
+      <circle
+        cx={16}
+        cy={16}
+        r={r}
+        opacity={1/8}
+      />
       <Box
-        ref={ref}
-        as="svg"
-        viewBox="0 0 32 32"
-        width={size}
-        height={size}
-        strokeWidth={strokeWidth}
-        fill="none"
-        stroke="currentcolor"
-        role="img"
-        {...props}
+        as='circle'
+        cx={16}
+        cy={16}
+        r={r}
+        strokeDasharray={C}
+        strokeDashoffset={offset}
         __css={{
-          color: 'primary',
-          overflow: 'visible',
-        }}>
-        <title>{title}</title>
-        <circle cx={16} cy={16} r={r} opacity={1 / 8} />
-        <Box
-          as="circle"
-          cx={16}
-          cy={16}
-          r={r}
-          strokeDasharray={C}
-          strokeDashoffset={offset}
-          __css={{
-            transformOrigin: '50% 50%',
-            animationName: spin.toString(),
-            animationTimingFunction: 'linear',
-            animationDuration: duration + 'ms',
-            animationIterationCount: 'infinite',
-          }}
-        />
-      </Box>
-    )
-  }
-)
+          transformOrigin: '50% 50%',
+          animationName: spin.toString(),
+          animationTimingFunction: 'linear',
+          animationDuration: duration + 'ms',
+          animationIterationCount: 'infinite',
+        }}
+      />
+    </Box>
+  )
+})
