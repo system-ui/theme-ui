e49688b0948fbe3cbda3fedd76b6106223db8858
Lachlan Campbell <lachlan@hackclub.com>
2020-01-23 15:13:20 -0500

    Continue variants implementation

--- packages/components/src/Box.js
+++ packages/components/src/Box.js
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled'
 import { css, get } from '@theme-ui/css'
-import { merge } from '@theme-ui/core'
 import { createShouldForwardProp } from '@styled-system/should-forward-prop'
 import space from '@styled-system/space'
 import color from '@styled-system/color'
+import deepmerge from 'deepmerge'
 
 const shouldForwardProp = createShouldForwardProp([
   ...space.propNames,
@@ -12,10 +12,17 @@ const shouldForwardProp = createShouldForwardProp([
 
 const sx = props => css(props.sx)(props.theme)
 const base = props => css(props.__css)(props.theme)
+
 const variant = ({ theme, variant, __themeKey = 'variants' }) =>
   css(get(theme, __themeKey + '.' + variant, get(theme, variant)))
-const variants = ({ theme, variants = [], __themeKey = 'variants' }) =>
-  css(merge(variants.map(v => get(theme, __themeKey + '.' + v, get(theme, v)))))
+const variants = ({ theme, variants = [], __themeKey = 'variants' }) => {
+  if (!Array.isArray(variants)) return {}
+  return css(
+    deepmerge.all(
+      variants.map(v => get(theme, __themeKey + '.' + v, get(theme, v)))
+    )
+  )
+}
 
 export const Box = styled('div', {
   shouldForwardProp,
