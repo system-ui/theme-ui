0678dd0f7aab6a99bd190bd779c2c79423096304
Krystian <dev@velein.pl>
2020-03-07 19:01:51 +0100

    docs(gatsby-theme-code-recipes): remove typo

--- packages/gatsby-theme-code-recipes/src/recipe.tsx
+++ packages/gatsby-theme-code-recipes/src/recipe.tsx
@@ -5,5 +5,5 @@ type Recipe = Omit<RecipeQuery, 'body'> & {
   children: React.ReactNode
 }
 
-/** Props such as a `name` etc. are passed down to and used by other packages */
+/** Props such as a `name` etc. are passed down and used by other packages */
 export default (props: Recipe) => <div>{props.children}</div>
