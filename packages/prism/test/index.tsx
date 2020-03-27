96e572cf767eccba406a5968781acc9ac2f2414c
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-03-27 10:14:56 +0100

    feat(@theme-ui/prism): convert to typescript

--- packages/prism/test/index.tsx
+++ packages/prism/test/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import renderer from 'react-test-renderer'
 import Prism from '../src'
 
@@ -6,7 +6,7 @@ const CODE = `
   console.log('hello, world!')
 `
 
-const render = el => renderer.create(el).toJSON()
+const render = (el: ReactElement) => renderer.create(el).toJSON()
 
 test('renders a code block', () => {
   const result = render(<Prism className="language-js">{CODE}</Prism>)
@@ -16,10 +16,7 @@ test('renders a code block', () => {
 
 test('renders with other languages', () => {
   const json = render(
-    <Prism
-      className='language-php'
-      children='<h1>Hello</h1>'
-    />
+    <Prism className="language-php" children="<h1>Hello</h1>" />
   )
   expect(json).toMatchSnapshot()
 })
