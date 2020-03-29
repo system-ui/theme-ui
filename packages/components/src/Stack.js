1687773231b6d81d57be4ffae551a125b2485e26
Jan Hoogeveen <jan@cleverfranke.com>
2020-03-29 15:41:39 +0200

    rewrite to use margin prop

--- packages/components/src/Stack.js
+++ packages/components/src/Stack.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import Box from './Box'
+import { Box } from './Box'
 
 export const Stack = React.forwardRef(
-  ({ gap = 2, flow = 'row', ...props }, ref) => {
+  ({ gap = 2, children, ...props }, ref) => {
+    /** Make a copy of the children so we can pass the styles as props instead of using brittle CSS selectors */
+    const elements = React.Children.map(children, (element, index) => {
+      if (index === 0) {
+        return element
+      }
+
+      const newProps = {
+        marginTop: gap,
+      }
+
+      return React.cloneElement(element, newProps)
+    })
+
     return (
-      <Box
-        ref={ref}
-        {...props}
-        __themeKey="stacks"
-        __css={{
-          display: 'grid',
-          gridGap: gap,
-          gridAutoFlow: flow,
-        }}
-      />
+      <Box ref={ref} {...props} __themeKey="stacks">
+        {elements}
+      </Box>
     )
   }
 )
