3a9581d0761082b139111503cec3a71c0d981f1b
Jan Hoogeveen <jan@cleverfranke.com>
2020-04-17 00:36:39 +0200

    stack as flex layout

--- packages/components/src/Stack.js
+++ packages/components/src/Stack.js
@@ -1,24 +1,61 @@
 import React from 'react'
 import { Box } from './Box'
 
+export const merge = (...objs) => {
+  return objs.reduce(function(merged, currentValue) {
+    return mergeTwo(merged, currentValue)
+  }, {})
+}
+
+const createGap = (direction, gap) => {
+  if (direction === 'vertical') {
+    return {
+      marginBottom: gap,
+      marginRight: 0,
+    }
+  }
+
+  return {
+    marginBottom: 0,
+    marginRight: gap,
+  }
+}
+
 export const Stack = React.forwardRef(
-  ({ gap = 2, children, ...props }, ref) => {
-    /** Make a copy of the children so we can pass the styles as props instead of using brittle CSS selectors */
-    const elements = React.Children.map(children, (element, index) => {
-      if (index === 0) {
-        return element
-      }
+  (
+    {
+      gap = 2,
+      direction = 'vertical',
+      inline = false,
+      justify = 'normal',
+      align = 'normal',
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    const styles = {
+      display: inline ? 'inline-flex' : 'flex',
+      width: '100%',
+      justifyContent: justify,
+      alignItems: align,
+    }
 
-      const newProps = {
-        marginTop: gap,
-      }
+    if (Array.isArray(direction)) {
+      styles.flexDirection = direction.map(d =>
+        d === 'vertical' ? 'column' : 'row'
+      )
+      styles['> *:not(:last-child)'] = direction.map(d => createGap(d, gap))
+    } else {
+      styles.flexDirection = direction === 'vertical' ? 'column' : 'row'
+      styles['> *:not(:last-child)'] = createGap(direction, gap)
+    }
 
-      return React.cloneElement(element, newProps)
-    })
+    console.log(styles)
 
     return (
-      <Box ref={ref} {...props} __themeKey="stacks">
-        {elements}
+      <Box ref={ref} {...props} __themeKey="stacks" __css={styles}>
+        {children}
       </Box>
     )
   }
