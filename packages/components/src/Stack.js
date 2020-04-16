c5202079e934076d01b303ba46d9bcf1ed534242
Jan Hoogeveen <jan@cleverfranke.com>
2020-04-16 22:33:48 +0200

    Use Grid instead of the owl selector

--- packages/components/src/Stack.js
+++ packages/components/src/Stack.js
@@ -2,24 +2,16 @@ import React from 'react'
 import { Box } from './Box'
 
 export const Stack = React.forwardRef(
-  ({ gap = 2, children, ...props }, ref) => {
-    /** Make a copy of the children so we can pass the styles as props instead of using brittle CSS selectors */
-    const elements = React.Children.map(children, (element, index) => {
-      if (index === 0) {
-        return element
-      }
-
-      const newProps = {
-        marginTop: gap,
-      }
-
-      return React.cloneElement(element, newProps)
-    })
-
-    return (
-      <Box ref={ref} {...props} __themeKey="stacks">
-        {elements}
-      </Box>
-    )
-  }
+  ({ gap = 2, children, ...props }, ref) => (
+    <Box
+      ref={ref}
+      {...props}
+      __themeKey="stacks"
+      __css={{
+        display: 'grid',
+        gridGap: gap,
+      }}>
+      {children}
+    </Box>
+  )
 )
