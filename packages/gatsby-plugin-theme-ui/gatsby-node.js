e16b8245faed80e639cb2e6ab40518cf38636875
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-04-30 14:35:59 -0400

    add dictionary to accept all strings

--- packages/gatsby-plugin-theme-ui/gatsby-node.js
+++ packages/gatsby-plugin-theme-ui/gatsby-node.js
@@ -1,13 +1,39 @@
+const prismPresetDictionary = {
+  'dracula': '@theme-ui/prism/presets/dracula.json',
+  'duotone-dark': '@theme-ui/prism/presets/duotone-dark.json',
+  'duotone-light': '@theme-ui/prism/presets/duotone-light.json',
+  'github': '@theme-ui/prism/presets/github.json',
+  'night-owl-light': '@theme-ui/prism/presets/night-owl-light.json',
+  'night-owl': '@theme-ui/prism/presets/night-owl.json',
+  'oceanic-next': '@theme-ui/prism/presets/oceanic-next.json',
+  'prism-coy': '@theme-ui/prism/presets/prism-coy.json',
+  'prism-dark': '@theme-ui/prism/presets/prism-dark.json',
+  'prism-funky': '@theme-ui/prism/presets/prism-funky.json',
+  'prism-solarizedlight': '@theme-ui/prism/presets/prism-solarizedlight.json',
+  'prism-tomorrow': '@theme-ui/prism/presets/prism-tomorrow.json',
+  'prism-twilight': '@theme-ui/prism/presets/prism-twilight.json',
+  'prism': '@theme-ui/prism/presets/prism.json',
+  'shades-or-purple': '@theme-ui/prism/presets/shades-or-purple.json',
+  'theme-ui': '@theme-ui/prism/presets/theme-ui.json',
+  'ultramin': '@theme-ui/prism/presets/ultramin.json',
+  'vs-dark': '@theme-ui/prism/presets/vs-dark.json',
+}
+
 exports.onPreInit = (__, options) => {
     let {themeModulePath} = options
     if(themeModulePath) {
         options.themeModulePath = require(themeModulePath)
     }
+
+    if(prismPreset in prismPresetDictionary) {
+      prismPreset = prismPresetDictionary[prismPreset]
+    }
+
     if(prismPreset) {
       try {
-        options.prismPreset = require(`@theme-ui/prism/presets/${prismPreset}.json`)
+        options.prismPreset = require(prismPreset)
       } catch {
-        reporter.error(`It appears the prism dependency is not installed. Try running \`${generateInstallInstructions()} @theme-ui/prism\``)
+        reporter.error(`It appears the prism dependency is not installed. Try running \`${generateInstallInstructions()}\n\n${prismPreset}\``)
       }
     }
 }
