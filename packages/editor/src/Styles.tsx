23d56436dbf559aa0726377542656956e2e6d359
Daniel Castillo <dcastillok@gmail.com>
2020-05-08 10:03:27 +0200

    convert Styles

--- packages/editor/src/Styles.tsx
+++ packages/editor/src/Styles.tsx
@@ -2,9 +2,10 @@
 import { jsx, useThemeUI } from 'theme-ui'
 import { Fragment } from 'react'
 import Sx from './Sx'
+import { EditorContext } from './types'
 
 export default ({ tag = 'root' }) => {
-  const context = useThemeUI()
+  const context = useThemeUI() as EditorContext
   const { styles = {} } = context.theme
 
   const style = styles[tag] || {}
@@ -23,19 +24,9 @@ export default ({ tag = 'root' }) => {
   return (
     <Fragment>
       <b>theme.styles.{tag}</b>
-      <Sx.Typography
-        value={style}
-        onChange={setStyle}
-        theme={context.theme}
-      />
-      <Sx.Margin
-        value={style}
-        onChange={setStyle}
-      />
-      <Sx.Colors
-        value={style}
-        onChange={setStyle}
-      />
+      <Sx.Typography value={style} onChange={setStyle} theme={context.theme} />
+      <Sx.Margin value={style} onChange={setStyle} />
+      <Sx.Colors value={style} onChange={setStyle} />
     </Fragment>
   )
 }
