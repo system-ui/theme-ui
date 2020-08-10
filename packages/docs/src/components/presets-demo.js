a59d608bb9fe40f964005cb28fb49af734978770
Lachlan Campbell <lachlan@hackclub.com>
2020-08-10 20:35:01 +0000

    fix(docs): Fix preset demo text color

--- packages/docs/src/components/presets-demo.js
+++ packages/docs/src/components/presets-demo.js
@@ -50,7 +50,7 @@ export default () => {
           </Select>
         </label>
         <ThemeContext.Provider value={preset}>
-          <Styled.root>
+          <Styled.root sx={{ bg: 'background', color: 'text' }}>
             <Styled.h2>Colors</Styled.h2>
             <ColorPalette omit={['modes', 'header']} />
             <Styled.h2>Typography</Styled.h2>
