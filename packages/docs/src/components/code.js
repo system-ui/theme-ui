91f420f8a84d53d2b13771c205e7a011421ced3b
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2020-11-02 08:10:34 +0100

    Merge pull request #1233 from lachlanjc/patch-6
    fix(docs): Fix example logo on Avatar & Image component docs

--- packages/docs/src/components/code.js
+++ packages/docs/src/components/code.js
@@ -42,7 +42,7 @@ const images = {
   flatiron:
     'https://images.unsplash.com/photo-1520222984843-df35ebc0f24d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
   logo:
-    'https://contrast.now.sh/fff/000?text=UI&size=96&fontSize=1.5&baseline=1',
+    'https://logo.clearbit.com/gatsbyjs.com',
 }
 
 const scope = {
