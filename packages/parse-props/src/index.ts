abbe8e670a44e522c6aa58291f08c3091829f21c
Daniel Castillo <daniel.castillo@uberall.com>
2020-11-05 18:32:44 +0100

    make parseProps more performant

--- packages/parse-props/src/index.ts
+++ packages/parse-props/src/index.ts
@@ -1,24 +1,20 @@
 import { InterpolationWithTheme } from '@emotion/core'
 import { css } from '@theme-ui/css'
 
-const getCSS = (props: any) => {
-  if (!props.sx && !props.css) return undefined
-  return (theme: any) => {
-    const styles = css(props.sx)(theme)
-    const raw = typeof props.css === 'function' ? props.css(theme) : props.css
-    return [styles, raw]
-  }
+const getCSS = (props: any) => (theme: any) => {
+  const styles = css(props.sx)(theme)
+  const raw = typeof props.css === 'function' ? props.css(theme) : props.css
+  return [styles, raw]
 }
 
 const parseProps = (props: any) => {
-  if (!props) return null
-  const next: typeof props & { css?: InterpolationWithTheme<any> } = {}
+  if (!props || (!props.sx && !props.css)) return props
+  const next: typeof props & { css: InterpolationWithTheme<any> } = {}
   for (let key in props) {
     if (key === 'sx') continue
     next[key] = props[key]
   }
-  const css = getCSS(props)
-  if (css) next.css = css
+  next.css = getCSS(props)
   return next
 }
 
