208ae9d549e02f0e3a859243366444431cc7fad9
hasparus <hasparus@gmail.com>
2020-11-18 13:40:32 +0100

    fix: tweak renderJSON to stop throwing on null

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -2,14 +2,10 @@ import renderer from 'react-test-renderer'
 
 export const renderJSON = (
   el: Parameters<typeof renderer.create>[0]
-): renderer.ReactTestRendererJSON => {
+): renderer.ReactTestRendererJSON | null => {
   const json = renderer.create(el).toJSON()
   if (Array.isArray(json)) {
     return json[0]
-  }
-
-  if (!json) {
-    throw new Error(`${json} was rendered by renderJSON`)
   }
 
   return json
