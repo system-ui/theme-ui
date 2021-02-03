c8841e9b8b8f1d211ae61f57775e416997d02919
Aaron Adams <aaron@aaronadams.ca>
2021-02-04 00:42:34 +0100

    Remove JSX pragma from gatsby-theme-style-guide
    Fixes `pragma and pragmaFrag cannot be set when runtime is automatic` when installing version `next` of Theme UI and this plugin.

--- packages/gatsby-theme-style-guide/src/typography.js
+++ packages/gatsby-theme-style-guide/src/typography.js
@@ -1,5 +1,5 @@
-/** @jsx jsx */
-import { jsx, Themed, useThemeUI } from 'theme-ui'
+import React from 'react'
+import { Themed, useThemeUI } from 'theme-ui'
 import { TypeScale, TypeStyle } from '@theme-ui/style-guide'
 
 const Row = (props) => (
