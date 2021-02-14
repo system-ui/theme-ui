19b74d9b17748c6a2d34242fc14d8c737a5ba048
hasparus <hasparus@gmail.com>
2021-02-14 23:07:32 +0100

    refactor(style-guide): update todo comment

--- packages/style-guide/src/TypeScale.tsx
+++ packages/style-guide/src/TypeScale.tsx
@@ -23,14 +23,13 @@ export const TypeScale = ({ reverse = true, ...props }) => {
         alignItems: 'baseline',
       }}>
       {fontSizeEntries.map(([key, val]) => {
-        if (typeof val === 'object') {
-          // TODO: can theme.fontSizes be a deeply nested object?
-          // This wasn't handled previously
+        if (!val || typeof val === 'object') {
+          // TODO: `theme.fontSizes` can be a deeply nested object.
           // We should either update the types or recursively render here.
           return null
         }
 
-          return (
+        return (
           <TypeStyle
             key={key}
             fontSize={key}
