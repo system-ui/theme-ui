3c2d250f50d1c1f044419f165890d2ec84a0bd00
Florent Schildknecht <florent.schildknecht@humblebee.se>
2021-03-08 16:57:15 +0100

    fix(components): remove hardcoded Paragraph style
    - remove hardcoded sx props (such style is easy to opt-in with the theme / variants, but harder to opt-out)

--- packages/components/src/Paragraph.js
+++ packages/components/src/Paragraph.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Box from './Box'
 
 export const Paragraph = React.forwardRef(function Paragraph(
-  { sx, ...props },
+  props,
   ref
 ) {
   return (
@@ -11,15 +11,6 @@ export const Paragraph = React.forwardRef(function Paragraph(
       as="p"
       variant="paragraph"
       {...props}
-      sx={{
-        // reset margin by default: avoid relying on user-agent margins (not aware of theme-ui space scale)
-        margin: 0,
-        // set a max-width: avoid full-page paragraphs
-        '@media screen and (min-width: 36em)': {
-          maxWidth: '48rem',
-        },
-        ...sx,
-      }}
       __themeKey="text"
       __css={{
         fontFamily: 'body',
