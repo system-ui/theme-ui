c03c825bb29284d781b5b64de83bea196507a11a
Aleksandra Sikora <aleksandra@hasura.io>
2021-04-02 20:04:47 +0200

    fix(css): borderTopWidth accepts number

--- packages/css/src/types.ts
+++ packages/css/src/types.ts
@@ -362,18 +362,6 @@ interface OverwriteCSSProperties {
    */
   borderTopStyle?: CSS.Property.BorderTopStyle | string
   /**
-   * The **`border-top-width`** CSS property sets the width of the top border of an element.
-   *
-   * **Initial value**: `medium`
-   *
-   * | Chrome | Firefox | Safari |  Edge  |  IE   |
-   * | :----: | :-----: | :----: | :----: | :---: |
-   * | **1**  |  **1**  | **1**  | **12** | **4** |
-   *
-   * @see https://developer.mozilla.org/docs/Web/CSS/border-top-width
-   */
-  borderTopWidth?: CSS.Property.BorderTopWidth<never> | string
-  /**
    * The **`border-bottom-style`** CSS property sets the line style of an element's bottom `border`.
    *
    * **Initial value**: `none`
