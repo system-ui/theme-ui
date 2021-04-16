8d64ad4a981ec1b82c3789c411cbd92ad2276c2a
Jonathan Van Buren <vanbujm@gmail.com>
2021-04-16 13:34:48 +0800

    fix(components) Updated Paragraph component to allow overriding the margin using styled-system space props

--- packages/components/src/Paragraph.js
+++ packages/components/src/Paragraph.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Box from './Box'
 
 export const Paragraph = React.forwardRef(function Paragraph(
-  { sx, ...props },
+  props,
   ref
 ) {
   return (
@@ -10,18 +10,15 @@ export const Paragraph = React.forwardRef(function Paragraph(
       ref={ref}
       as="p"
       variant="paragraph"
-      {...props}
-      sx={{
-        // reset margin by default: avoid relying on user-agent margins (not aware of theme-ui space scale)
-        margin: 0,
-        ...sx
-      }}
+      // reset margin by default: avoid relying on user-agent margins (not aware of theme-ui space scale)
+      m={0}
       __themeKey="text"
       __css={{
         fontFamily: 'body',
         fontWeight: 'body',
         lineHeight: 'body',
       }}
+      {...props}
     />
   )
 })
