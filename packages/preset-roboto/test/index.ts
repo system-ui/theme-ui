f89ba25e120aaba4e9a281adf2fdabf465f84853
Torne Wuff <torne@wolfpuppy.org.uk>
2021-07-04 15:27:44 -0400

    Define as actual tests to satisfy jest

--- packages/preset-roboto/test/index.ts
+++ packages/preset-roboto/test/index.ts
@@ -2,7 +2,8 @@ import { Theme } from '@theme-ui/css'
 
 import theme from '../src'
 
-// No actual test executed here; just verify it typechecks.
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const typedTheme: Theme = theme
+// Nothing actually executed here; just verify it typechecks.
+test('assignable to Theme', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const typedTheme: Theme = theme
+})
