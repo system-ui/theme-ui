bc7f2d8bd681d1fb742efc1b37833f547d0c3b6a
hasparus <hasparus@gmail.com>
2021-07-18 17:01:39 +0200

    fix(core): add `css` prop back to JSX types

--- packages/css/emotion-theme.d.ts
+++
@@ -1,9 +0,0 @@
-import '@emotion/react'
-
-import { Theme } from '@theme-ui/css'
-
-interface ThemeUITheme extends Theme {}
-
-declare module '@emotion/react' {
-  export interface Theme extends ThemeUITheme {}
-}
