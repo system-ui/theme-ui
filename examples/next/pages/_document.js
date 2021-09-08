95615eae3b31422ab22e2694e05817f5b0a350ab
hasparus <hasparus@gmail.com>
2021-09-08 08:51:02 +0200

    docs(examples/next): showcase automatic runtime pragma comment in document.js

--- examples/next/pages/_document.js
+++ examples/next/pages/_document.js
@@ -1,3 +1,5 @@
+/** @jsxRuntime automatic */
+/** @jsxImportSource theme-ui */
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { InitializeColorMode } from 'theme-ui'
 
@@ -8,14 +10,16 @@ export default class MyDocument extends Document {
   }
 
   render() {
-    console.log('Document renders')
-
     return (
       <Html>
         <Head />
         <body>
           <InitializeColorMode />
-          <Main />
+          <div
+            sx={{ border: '1px solid', borderColor: 'text', padding: '1em' }}
+          >
+            <Main />
+          </div>
           <NextScript />
         </body>
       </Html>
