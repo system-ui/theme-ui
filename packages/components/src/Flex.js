0dfdffbeee1b05472949612e898ffdddf0d0195d
Lachlan Campbell <lachlanjc@hey.com>
2021-10-27 00:37:38 -0700

    Add gap prop to Flex

--- packages/components/src/Flex.js
+++ packages/components/src/Flex.js
@@ -1,8 +1,20 @@
-import styled from '@emotion/styled'
+import React from 'react'
 import Box from './Box'
 
-export const Flex = styled(Box)({
-  display: 'flex',
+export const Flex = React.forwardRef(function Flex(
+  { gap, ...props },
+  ref
+) {
+  return (
+    <Box
+      ref={ref}
+      {...props}
+      __css={{
+        display: 'flex',
+        gap,
+      }}
+    />
+  )
 })
 
 Flex.displayName = 'Flex'
