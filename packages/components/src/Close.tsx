16c1fe9348071fbabe23c29f4e6cd6b5cafa2061
Lachlan Campbell <lachlanjc@hey.com>
2022-01-08 18:29:53 -0500

    Export CloseIcon

--- packages/components/src/Close.tsx
+++ packages/components/src/Close.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 import { IconButton, IconButtonProps } from './IconButton'
 import type { ForwardRef } from './types'
 
-const X_ICON = (
+export const CloseIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     fill="currentcolor"
-    viewBox="0 0 24 24">
+    viewBox="0 0 24 24"
+  >
     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
   </svg>
 )
@@ -34,7 +35,7 @@ export const Close: ForwardRef<HTMLButtonElement, CloseProps> =
         aria-label="Close"
         variant="close"
         {...props}
-        children={X_ICON}
+        children={CloseIcon}
       />
     )
   })
