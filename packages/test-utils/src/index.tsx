60dcc6d530b1c56f28c804ebe235d1ce543973b2
hasparus <hasparus@gmail.com>
2022-04-08 11:58:51 +0200

    test(test-utils): add renderHook function

--- packages/test-utils/src/index.tsx
+++ packages/test-utils/src/index.tsx
@@ -1,5 +1,8 @@
+import * as React from 'react'
+import { render } from '@testing-library/react'
 import renderer from 'react-test-renderer'
 import * as tsSnippet from 'ts-snippet'
+import { Theme, ThemeProvider } from 'theme-ui'
 
 export * from '@testing-library/react'
 
@@ -15,6 +18,31 @@ export type {
   IsUnknown,
   NotHas,
 } from 'conditional-type-checks'
+
+export const renderHook = <T,>(
+  useHook: () => T,
+  options: { theme?: Theme } = {}
+) => {
+  let value: T | undefined = undefined
+
+  const Component = () => {
+    value = useHook()
+    return null
+  }
+
+  const { theme } = options
+  const { unmount, rerender } = render(<Component />, {
+    wrapper: theme && ((props) => <ThemeProvider theme={theme} {...props} />),
+  })
+
+  return {
+    unmount,
+    rerender,
+    get result() {
+      return value!
+    },
+  }
+}
 
 export const renderJSON = (
   el: Parameters<typeof renderer.create>[0]
