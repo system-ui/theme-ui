acb51915698740f8e71e0c589ecfc80b02f777fd
hasparus <hasparus@gmail.com>
2022-08-29 14:23:18 +0200

    chore(deps): add a pnpm hook to ensure @emotion/react is installed just once

---
+++ .pnpmfile.cjs
@@ -0,0 +1,40 @@
+const PACKAGES_WITH_ENFORCED_SINGLE_VERSION = ['@emotion/react']
+
+function afterAllResolved(lockfile, context) {
+  context.log(`Checking duplicate packages`)
+
+  const packagesKeys = Object.keys(lockfile.packages)
+  const found = {}
+  for (let p of packagesKeys) {
+    for (let x of PACKAGES_WITH_ENFORCED_SINGLE_VERSION) {
+      if (p.startsWith(`/${x}/`)) {
+        if (found[x]) {
+          found[x] += 1
+        } else {
+          found[x] = 1
+        }
+      }
+    }
+  }
+
+  let msg = ''
+
+  for (let p in found) {
+    const count = found[p]
+    if (count > 1) {
+      msg += `${p} found ${count} times\n`
+    }
+  }
+
+  if (msg) {
+    throw new Error(msg)
+  }
+
+  return lockfile
+}
+
+module.exports = {
+  hooks: {
+    afterAllResolved,
+  },
+}
