086f550713d8258e04247549ac3f21af4ace697f
Lachlan Campbell <lachlanjc@hey.com>
2022-11-03 21:32:33 -0400

    Switch to "default" fontSize

--- packages/preset-tailwind/src/index.ts
+++ packages/preset-tailwind/src/index.ts
@@ -416,7 +416,7 @@ export const fonts = {
 export const fontSizes = {
   xs: '0.75rem',
   sm: '0.875rem',
-  base: '1rem',
+  default: '1rem',
   lg: '1.125rem',
   xl: '1.25rem',
   '2xl': '1.5rem',
