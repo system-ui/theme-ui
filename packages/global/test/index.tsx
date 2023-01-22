88378258d5322579e962aca7fdec80b97d42ca0f
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-01-22 18:32:09 +0100

    test(global): change the test a bit to make it work consistently

--- packages/global/test/index.tsx
+++ packages/global/test/index.tsx
@@ -56,14 +56,12 @@ test.only('renders global styles', async () => {
   const document = render(root)
   expect(document.baseElement.parentElement).toMatchSnapshot()
 
-  const style = window.getComputedStyle(document.baseElement.parentElement!)
-  expect(style.fontFamily).toBe('Georgia,serif')
-  expect(style.margin).toBe('0px')
+  const bodyStyle = global.getComputedStyle(document.baseElement)
+  expect(bodyStyle.fontFamily).toBe('Georgia,serif')
+  expect(bodyStyle.margin).toBe('0px')
 
   const h1 = document.baseElement.querySelector('h1')!
-  const h1Style = global.getComputedStyle(h1)
-  expect(h1Style.fontFamily).toBe('Georgia,serif')
-  expect(h1Style.color).toBe('salmon')
+  expect(global.getComputedStyle(h1).color).toBe('salmon')
 })
 
 test('renders global and component styles', () => {
