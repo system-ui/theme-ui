a9dba9233974e058e9b0fee385de9a692c844603
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-01-22 17:44:00 +0100

    Add supressHydrationWarning to colorMode toggle —  ofc server content differs

--- packages/global/test/index.tsx
+++ packages/global/test/index.tsx
@@ -20,7 +20,7 @@ beforeEach(() => {
 
 afterEach(cleanup)
 
-test('renders global styles', async () => {
+test.only('renders global styles', async () => {
   const root = (
     <ThemeProvider
       theme={{
@@ -56,14 +56,14 @@ test('renders global styles', async () => {
   const document = render(root)
   expect(document.baseElement.parentElement).toMatchSnapshot()
 
-  // const style = window.getComputedStyle(document.baseElement.parentElement!)
-  // expect(style.fontFamily).toBe('Georgia,serif')
-  // expect(style.margin).toBe('0px')
+  const style = window.getComputedStyle(document.baseElement.parentElement!)
+  expect(style.fontFamily).toBe('Georgia,serif')
+  expect(style.margin).toBe('0px')
 
-  // const h1 = document.baseElement.querySelector('h1')!
-  // const h1Style = global.getComputedStyle(h1)
-  // expect(h1Style.fontFamily).toBe('Georgia,serif')
-  // expect(h1Style.color).toBe('salmon')
+  const h1 = document.baseElement.querySelector('h1')!
+  const h1Style = global.getComputedStyle(h1)
+  expect(h1Style.fontFamily).toBe('Georgia,serif')
+  expect(h1Style.color).toBe('salmon')
 })
 
 test('renders global and component styles', () => {
